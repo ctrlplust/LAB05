6acc92fd61984ccce97ccf88649f835a881c0300
Tplus <166980259+ctrlplust@users.noreply.github.com>
2025-06-28 18:02:07 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -53,7 +53,7 @@ public class Player {
     
 // Dejo esto como comentario ya que no es necesario dejarlo implementado en el codigo, Es solo para un futuro si se quiere cambiar el diseño del codigo
 // como ejemplo cuando vas a comparar jugadores  con con contains(new Player("Nombre")) ,  para tambien sobre escribir el Hash Code.
-// y el toString es para la depuracion, para las pruebas y mostrar informacion del objeto mas legible
+// y el toString es para la depuracion, para las pruebas y mostrar informacion del objeto mas legible, Se pueden agregar por que se como se ocupan los override y estos toString, se descartan para no tener problemas con la evaluacion
     
     // se sobreescribe el metodo equals para poder definir cuando dos objetos Player son iguales
     // en este caso, dos objetos Player son iguales si tienen el mismo nombre de jugador
